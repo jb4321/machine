cf538ae220d9910247c7abb0452e2276fa635a1b
jb4321 <67065206+jb4321@users.noreply.github.com>
2023-05-18 20:39:21 +0200

    Moving

--- main.cpp
+++ main.cpp
@@ -3,34 +3,62 @@
 #include <string>
 
 #include <pdcurses.h>
-
 //zmienne
-int x = 0;
-int y = 0;
+int x = 20;
+int y = 10;
+int term_x = 60;
+int term_y = 30;
 
+bool IsNotOut(int y_, int x_)
+{
+    if(x_ >= term_x || x_ < 0)
+    {
+        return false;
+    }
+    if(y_ >= term_y || y_ < 0)
+    {
+        return false;
+    }
+    return true;
+}
 int main()
 {
     //start
+    std::cout << "\x1b]20;" << "10x20" << "\a" << std::flush;
+    std::cout << "test";
     initscr();
+    raw();
+    resize_term(term_y,term_x);
+    if(has_colors() == FALSE)
+	{	endwin();
+		printf("Your terminal does not support color\n");
+		exit(1);
+	}
+    start_color();
+    init_pair(1,COLOR_RED,COLOR_BLACK);
+
     printw( "Press Button..\n" );
     keypad(stdscr, TRUE);	
     noecho();
+    move(y,x);
     do
-    {  
+    {
+        int ny=y;
+        int nx=x; 
         int key = getch();
         switch (key)
         {
             case KEY_UP:
-                y--;
+                ny--;
                 break;
             case KEY_DOWN:
-                y++;
+                ny++;
                 break;
             case KEY_LEFT:
-                x--;
+                nx--;
                 break;
             case KEY_RIGHT:
-                x++;
+                nx++;
                 break;
             default:
                 break;
@@ -39,6 +67,22 @@ int main()
         {
             break;
         }
+        
+        
+        if(IsNotOut(ny,nx))
+        {
+            x = nx;
+            y = ny;
+        }
+        mvprintw(y,x,"@");
+        
+        std::string txt = std::to_string(x) + "," + std::to_string(y) + " " ;
+        attron(A_BOLD);
+        attron(COLOR_PAIR(1));
+        mvprintw(0,0,txt.c_str());
+        attroff(A_BOLD);
+        attroff(COLOR_PAIR(1));
+        
         move(y,x);
     } while (true);
     
